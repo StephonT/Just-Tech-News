d70be3e7435c05d5c20b92687778598ebe4af6de
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-07-30 14:03:40 -0400

    Update server.js
    added comment

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(routes);
 
 
 // turn on connection to db and server
+//forcing the sync method to true, we will make the tables re-create if there are any association changes.
 sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
