0dbc45128a883b58393747829b3e0b2eb36fe451
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-07-30 13:56:40 -0400

    Update index.js
    Created associations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// create associations
+
+//User can have many post
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+//A post belongs to one user
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
